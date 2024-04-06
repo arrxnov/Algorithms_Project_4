7997eb76840072aeebc325a5602c360e2642d5e9
arrxnov <jcbgrd@gmail.com>
2024-04-05 22:23:07 -0400

    Push more scaffolding and fetching input.

--- Algorithms_Project_4/main.cpp
+++ Algorithms_Project_4/main.cpp
@@ -3,13 +3,32 @@
 #include <queue>
 #include <map>
 
+//======================//
+//        CLASSES       //
+//======================//
+
 class Move
 {
 public:
 	std::string board;
 	std::string moveString;
 	Move* parent;
+	Move() 
+	{
+		board = "";
+		moveString = "";
+		parent = NULL;
+	};
+	Move(std::string newBoard, std::string newMoveString, Move* newParent) : 
+		board(newBoard),
+		moveString(newMoveString),
+		parent(newParent) {}
+
 };
+
+//======================//
+//   HELPER FUNCTIONS   //
+//======================//
 
 bool isSolved(std::string board)
 {
@@ -21,16 +40,59 @@ bool isSolved(std::string board)
 	}
 }
 
+//======================//
+//         MAIN         //
+//======================//
+
 int main()
 {
 	using namespace std;
 	
-	string board, startingBoard;
-	queue<string> moves;
+	int numCars;
+
+	string board = "", startingBoard = "";
+	queue<Move> moves;
 	map<string, int> previousPositions;
 	map<string, int> colorsToNums; // convenience really
 
 	// read in all input
+
+	cin >> numCars;
+
+	for (int i = 0; i < numCars; i++)
+	{
+		string type, color;
+		char orientation;
+		int col, row;
+		cin >> type >> color >> orientation >> col >> row;
+
+		// Insert color into the convenience map
+		colorsToNums.insert(pair<string, int>(color, i));
+
+		// Insert automobile onto the board
+		if (type == "car")
+		{
+			if (orientation == 'h')
+			{
+
+			}
+			else if (orientation == 'v')
+			{
+
+			}
+		}
+		if (type == "truck")
+		{
+			if (orientation == 'h')
+			{
+
+			}
+			else if (orientation == 'v')
+			{
+
+			}
+		}
+	}
 
 	do 
 	{
@@ -49,5 +111,7 @@ int main()
 		}
 
 		// add derivative moves to queue
+		// blackMagic();
+
 	} while (!moves.empty());
 }
