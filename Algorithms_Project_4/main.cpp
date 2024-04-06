a75ca2937a0603212518e56436fb682039c70f65
arrxnov <jcbgrd@gmail.com>
2024-04-05 22:08:07 -0400

    Start scaffolding code

--- Algorithms_Project_4/main.cpp
+++ Algorithms_Project_4/main.cpp
@@ -3,10 +3,51 @@
 #include <queue>
 #include <map>
 
+class Move
+{
+public:
+	std::string board;
+	std::string moveString;
+	Move* parent;
+};
+
+bool isSolved(std::string board)
+{
+	for (int i = 5; i >= 0; i--)
+	{
+		if (board[i*6+3] > 1) return false;
+		else if (board[i*6+3] == 1) return true;
+		else continue;
+	}
+}
+
 int main()
 {
+	using namespace std;
 	
+	string board, startingBoard;
+	queue<string> moves;
+	map<string, int> previousPositions;
+	map<string, int> colorsToNums; // convenience really
 
+	// read in all input
 
-	return 0;
+	do 
+	{
+		if (isSolved(board))
+		{
+			// print the solution by going thru backards
+
+			// exit gracefully
+			return 0;
+		}
+
+		if (previousPositions.find(board) != previousPositions.end())
+		{
+			// if we've been here before skip it and move on
+			continue;
+		}
+
+		// add derivative moves to queue
+	} while (!moves.empty());
 }
